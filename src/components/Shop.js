e88cc85508d9ef1cb4e93f6aab908b4546972374
jkurthoconnor <jkurthoconnor@gmail.com>
2019-06-11 11:32:44 -0600

    Add get all api call

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,41 +1,28 @@
 import React, { Component } from 'react';
 import ShoppingCart from './ShoppingCart.js'
 import ProductsWrapper from './ProductsWrapper.js'
+import client from '../lib/client';
+
 class Shop extends Component {
-  render() {
-
-  let products = [
-  {
-    id: 1,
-    title: 'Amazon Kindle E-reader',
-    quantity: 5,
-    price: 79.99
-  },
-  {
-    id: 2,
-    title: 'Apple 10.5-Inch iPad Pro',
-    quantity: 3,
-    price: 649.99
-  },
-  {
-    id: 3,
-    title: 'Yamaha Portable Keyboard',
-    quantity: 2,
-    price: 155.99
-  },
-  {
-    id: 4,
-    title: 'Tinker, Tailor, Soldier, Spy - A John le Carre Novel',
-    quantity: 12,
-    price: 13.74
+  state = {
+    products: [],
   }
-  ]
 
+  componentDidMount() {
+      client.get('/api/products')
+            .then((products) => {
+      this.setState({
+        products
+      })
+    })
+  }
+
+  render() {
     return (
      <div>
       <ShoppingCart />
       <ProductsWrapper
-       products={products}
+       products={this.state.products}
       />
      </div>
     );
