5c87bfd199d84b26e4615b418e5e76bbe73e5395
jkurthoconnor <jkurthoconnor@gmail.com>
2019-06-13 22:33:25 -0600

    Stock reduces with addition to cart

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,21 +1,33 @@
 const productReducer = ((state=[], action) => {
   switch (action.type) {
     case 'FETCH_PRODUCTS':
-      return state.concat(action.products)
+      return state.concat(action.products);
     case 'ADD_PRODUCT':
-      return state.concat(action.product)
+      return state.concat(action.product);
     case 'UPDATE_PRODUCT':
       return state.map((product) => {
         if (product.id === action.product.id) {
-          return {...action.product}
+          return {...action.product};
         } else {
-          return product
+          return product;
         }
       })
     case 'DELETE_PRODUCT':
       return state.filter((product) => {
-        return product.id !== action.id
+        return product.id !== action.id;
       })
+    case 'ADD_ITEM_TO_CART':
+      let value = state.map( product => {; 
+        if (product.id === action.item.id) {
+          return {...product, quantity: product.quantity - 1 };
+        } else {
+        return product;
+        }
+      })
+
+      console.log(value);
+      return value;
+
     default:
       return state;
   }
